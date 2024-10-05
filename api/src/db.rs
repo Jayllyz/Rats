4ef37473efde55bd1fb1b42ecf7c6685e4577c45
Antony David <antonydavid945@gmail.com>
2024-10-05 11:00:53 +0200

    feat: setup rust api (#4)

---
+++ api/src/db.rs
@@ -0,0 +1,26 @@
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    PgConnection,
+};
+use dotenv::dotenv;
+use std::env;
+
+pub type DbPool = Pool<ConnectionManager<PgConnection>>;
+
+pub fn establish_connection() -> DbPool {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder().build(manager).expect("Failed to create db pool.")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_database_connection() {
+        let db_pool = establish_connection().get();
+        assert!(db_pool.is_ok(), "Should be able to get a connection from the pool");
+    }
+}

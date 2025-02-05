1a60fc0539119b540a45647d5ebe749c2a20ee32
Simon <szhang0709@gmail.com>
2025-02-05 16:32:07 +0100

    refactor: modified lines

--- api/migrations/2025-02-05-144717_insert_metro_lines/down.sql
+++ api/migrations/2025-02-05-144717_insert_metro_lines/down.sql
@@ -1,5 +1,5 @@
 -- This file should undo anything in `up.sql`
 DELETE FROM train_lines
-WHERE name IN ('Ligne 1', 'Ligne 2', 'Ligne 3', 'Ligne 3bis', 'Ligne 4', 
-                'Ligne 5', 'Ligne 6', 'Ligne 7', 'Ligne 7bis', 'Ligne 8', 
+WHERE name IN ('Ligne 1', 'Ligne 2', 'Ligne 3', 'Ligne 4', 
+                'Ligne 5', 'Ligne 6', 'Ligne 7', 'Ligne 8', 
                 'Ligne 9', 'Ligne 10', 'Ligne 11', 'Ligne 12', 'Ligne 13', 'Ligne 14');

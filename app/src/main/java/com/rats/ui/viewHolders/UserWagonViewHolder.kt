93edbf48b6c4570896ae1ba1a11f94118d28b71b
Metololo <93775692+Metololo@users.noreply.github.com>
2025-02-05 14:52:58 +0100

    fix: import

--- app/src/main/java/com/rats/ui/viewHolders/UserWagonViewHolder.kt
+++ app/src/main/java/com/rats/ui/viewHolders/UserWagonViewHolder.kt
@@ -7,6 +7,6 @@ import com.rats.R
 
 class UserWagonViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
     val name = itemView.findViewById<TextView>(R.id.tv_user_name)
-    val email = itemView.findViewById<TextView>(R.id.tv_user_email)
+    val email = itemView.findViewById<TextView>(R.id.tv_star)
     val rootView: View = itemView
 }

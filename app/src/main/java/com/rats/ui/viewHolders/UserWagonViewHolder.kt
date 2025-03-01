51d2db51fdfedac08b4cfb6e754f1f4b627dd31f
Antony David <antonydavid945@gmail.com>
2025-03-01 10:44:17 +0100

    refactor: improve my wagon design a bit

--- app/src/main/java/com/rats/ui/viewHolders/UserWagonViewHolder.kt
+++ app/src/main/java/com/rats/ui/viewHolders/UserWagonViewHolder.kt
@@ -5,7 +5,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.rats.R
 
-class UserWagonViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+class UserWagonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val name: TextView = itemView.findViewById(R.id.tv_user_name)
     val email: TextView = itemView.findViewById(R.id.tv_star)
     val rootView: View = itemView
